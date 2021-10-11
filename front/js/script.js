166b19870b1c1808103da422183229f0ce6af56a
Lea <lea.delaporte96@hotmail.fr>
2021-10-11 18:33:36 +0200

    Ajout dans le DOM ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,30 @@
-// Récupération des articles dans l'API //
 
-async function getArticles() {
+
+// Insertion des produits dans le DOM // 
+function display (res){
+    console.log("Je passe dans display");
+    let divDisplay = document.getElementById("items");
+    for (let i = 0; i < res.length; i++){
+        console.log("Je passe dans la boucle");
+        divDisplay.innerHTML += `
+        <a href="./product.html?id=${res[i]._id}">
+            <article>
+                <img src="${res[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+                <h3 class="productName">${res[i].name}</h3>
+                <p class="productDescription">${res[i].description} </p>
+            </article>
+        </a>`;
+    }
+}
+
+// Récupération des articles dans l'API //
+function getArticles() {
     fetch("http://localhost:3000/api/products")
         .then(response => response.json())
         .then(data => { 
+            console.log("Je suis passé par là");
             console.log(data);
+            display(data);
         })
     
         .catch(function (err) {
@@ -13,30 +33,3 @@ async function getArticles() {
 }
 
 getArticles();
-
-// Insertion des produits dans le DOM // 
-
-function display (res){
-    let divDisplay = document.getElementById("items");
-    for (let i = 0; i < res; i++){
-        divDisplay.innerHTML `
-        <a href="./product.html?id=${res[i]._id}">
-            <article>
-                <img src="${res[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-                <h3 class="productName">${res[i].name}</h3>
-                <p> class="productDescription">${res[i].description}</p>
-            </article>
-        </a>`;
-
-    }
-}
-
-function saveIn(items){
-    localStorage.setItem("items",JSON.stringify(items));
-}
-
-async function main(){
-    const items = await getArticles();
-}
-
-main();
