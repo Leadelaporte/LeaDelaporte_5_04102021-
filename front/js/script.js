3a4ac45814ef0b5b956f1af760fbc42cc37f2d8a
Lea <lea.delaporte96@hotmail.fr>
2021-10-11 16:02:15 +0200

    insertion DOM

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
+// Récupération des articles dans l'API //
 
-
-function getArticles() {
+async function getArticles() {
     fetch("http://localhost:3000/api/products")
         .then(response => response.json())
         .then(data => { 
@@ -13,3 +13,30 @@ function getArticles() {
 }
 
 getArticles();
+
+// Insertion des produits dans le DOM // 
+
+function display (res){
+    let divDisplay = document.getElementById("items");
+    for (let i = 0; i < res; i++){
+        divDisplay.innerHTML `
+        <a href="./product.html?id=${res[i]._id}">
+            <article>
+                <img src="${res[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+                <h3 class="productName">${res[i].name}</h3>
+                <p> class="productDescription">${res[i].description}</p>
+            </article>
+        </a>`;
+
+    }
+}
+
+function saveIn(items){
+    localStorage.setItem("items",JSON.stringify(items));
+}
+
+async function main(){
+    const items = await getArticles();
+}
+
+main();
